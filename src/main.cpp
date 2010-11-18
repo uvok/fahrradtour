e1982d9c7ba3ae532ad6d241f5a4f9deec83adb8
uvok <uvok@online.de>
2010-11-18 21:04:28 +0100

    main.cpp: Test program

--- src/main.cpp
+++ src/main.cpp
@@ -8,35 +8,51 @@
 #include "Date.h"
 #include "Time.h"
 #include "Tour.h"
+#include "file.h"
+#include <cstdio>
+
+
+bool createTour();
 
 int main() {
+    //createTour();
+    return 0;
+}
+
+bool createTour() {
     Time time;
     Date date;
     Tour tour;
 
     std::string dat, dur;
-
     float maxsp, leng;
 
-    std::cout << "Datum" << std::endl;
-    std::cin>> dat;
+    do {
+        std::cout << "Datum" << std::endl;
+        std::cin >> dat;
+    } while (date.setDate(dat) != true);
 
     std::cout << "Laenge" << std::endl;
-    std::cin>>leng ;
+    std::cin >> leng;
 
-    std::cout << "Dauer" << std::endl;
-    std::cin>> dur;
+    do {
+        std::cout << "Dauer" << std::endl;
+        std::cin >> dur;
+    } while (time.setTime(dur) != true);
 
     std::cout << "Max Geschwindigkeit" << std::endl;
-    std::cin>> maxsp;
-
-    date.setDate(dat);
-    time.setTime(dur);
+    std::cin >> maxsp;
 
     tour.setupTour(&date, &time, leng, maxsp);
 
     tour.dumpData();
 
+    char cor;
+    do {
+        std::cout << "Daten korrekt? <y/n> ";
+        cor = getchar();
+        std::cin.ignore(1, '\n');
+    } while (cor != 'y' && cor != 'n');
 
-    return 0;
+    return true;
 }
