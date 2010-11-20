35c78dbf67123344f0dddd4336ce280cce450beb
uvok <uvok@online.de>
2010-11-20 16:14:11 +0100

    * fixed formatting  * using cerr instead of cout  * Commenting

--- src/Date.cpp
+++ src/Date.cpp
@@ -8,7 +8,7 @@
 #include "Date.h"
 
 Date::Date() :
-Day(1), Month(1), Year(1970) {
+    Day(1), Month(1), Year(1970) {
 }
 
 Date::Date(const std::string& datum) {
@@ -19,13 +19,15 @@ Date::~Date() {
 }
 
 std::string Date::getFormatDate() {
-    std::string datestring="";
+    // TODO: Could do it with outstreams
+    // wouldn't have to do that string appending stuffs
+    std::string datestring = "";
 
-    if(Day<10) datestring += "0";
+    if (Day < 10) datestring += "0";
     datestring.append(IntToString(Day));
     datestring.append(".");
 
-    if(Month<10) datestring += "0";
+    if (Month < 10) datestring += "0";
     datestring.append(IntToString(Month));
     datestring.append(".");
     datestring.append(IntToString(Year));
@@ -34,44 +36,61 @@ std::string Date::getFormatDate() {
 }
 
 bool Date::setDate(const std::string& date) {
-    if(date.length()!=10) {std::cout << "Formatfehler"; return false;}
+    // Same here, erstmal nur prüfen ob Stringlänge stimmt.
+    if (date.length() != 10) {
+        std::cerr << "Formatfehler" << std::endl;
+        return false;
+    }
 
     std::stringstream ss(date);
 
     std::vector<short> v;
-    short i=0;
+    short i = 0;
 
     std::string j;
 
-    while(ss.good()) {
-        std::getline( ss, j, '.');
+    while (ss.good()) {
+        std::getline(ss, j, '.');
         i = StringToInt(j);
         v.push_back(i);
     }
 
-    if(v.size()!=3) {std::cout << "Formatfehler"; return false;}
+    if (v.size() != 3) {
+        std::cerr << "Formatfehler" << std::endl;
+        return false;
+    }
 
     // 1. Year
 
     i = v.back();
-    // Jahresgrenzen willkuerlich festgelegt
-    if(i>2000 && i<2100) Year = i;
-    else {std::cout << "Formatfehler"; return false;}
     v.pop_back();
+    // Jahresgrenzen willkuerlich festgelegt
+    if (i > 2000 && i < 2100) Year = i;
+    else {
+        std::cerr << "Formatfehler" << std::endl;
+        return false;
+    }
 
     // 2. Month
 
     i = v.back();
-    if(i>0 && i<13) Month = i;
-    else {std::cout << "Formatfehler"; return false;}
     v.pop_back();
+    if (i > 0 && i < 13) Month = i;
+    else {
+        std::cerr << "Formatfehler" << std::endl;
+        return false;
+    }
 
     // 3. Day
 
     i = v.back();
-    if(i>0 && i<32) Day = i;
-    else {std::cout << "Formatfehler"; return false;}
     v.pop_back();
+
+    if (i > 0 && i < 32) Day = i;
+    else {
+        std::cerr << "Formatfehler" << std::endl;
+        return false;
+    }
 
     return true;
 }
