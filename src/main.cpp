7a09ee5c982ac55662697ce66e5bc9fbc26caf37
uvok <uvok@online.de>
2010-11-20 17:55:42 +0100

    Use stack for Date and Time when creating tour

--- src/main.cpp
+++ src/main.cpp
@@ -10,50 +10,70 @@
 #include "Tour.h"
 #include "file.h"
 #include <cstdio>
+#include <cctype>
 
-bool createTour();
+bool create();
 
 int main() {
-    //createTour();
+    // Menu
+    // 1. Create Tour
+    // 2. Dump Tours
+    // 3. (Future) Get statistics
+
+    create();
     return 0;
+
 }
 
-bool createTour() {
+bool create() {
     // Man koennte meinen, dies gehoert in die Tour Klasse,
     // Sehe ich nicht so, da hier die Daten nur abgefragt werden...
-    Time time;
-    Date date;
+
+    Time* time = new Time();
+    Date* date = new Date();
     Tour tour;
 
     std::string dat, dur;
     float maxsp, leng;
 
     do {
-        std::cout << "Datum" << std::endl;
+        std::cout << "Datum [DD.MM.YYYY]: " << std::endl;
         std::cin >> dat;
-    } while (date.setDate(dat) != true);
+    } while (date->setDate(dat) != true);
 
-    std::cout << "Laenge" << std::endl;
+    std::cout << "Laenge: " << std::endl;
     std::cin >> leng;
 
     do {
-        std::cout << "Dauer" << std::endl;
+        std::cout << "Dauer [MM:SS]: " << std::endl;
         std::cin >> dur;
-    } while (time.setTime(dur) != true);
+    } while (time->setTime(dur) != true);
 
-    std::cout << "Max Geschwindigkeit" << std::endl;
+    std::cout << "Max Geschwindigkeit: " << std::endl;
     std::cin >> maxsp;
 
-    tour.setupTour(&date, &time, leng, maxsp);
+    tour.setupTour(date, time, leng, maxsp);
 
-    tour.dumpData();
+    tour.dumpFormatData();
 
     char cor;
     do {
-        std::cout << "Daten korrekt? <y/n> ";
+        std::cout << "Daten korrekt und Daten speichern? <y/n> ";
+        std::cin.ignore(1, '\n');
         cor = getchar();
         std::cin.ignore(1, '\n');
+        //cor = std::tolower(cor);
     } while (cor != 'y' && cor != 'n');
+
+    if (cor == 'n') {
+        delete time;
+        delete date;
+        return false;
+    }
+    else if (cor == 'y') saveToFile(&tour);
+
+    delete time;
+    delete date;
 
     return true;
 }
